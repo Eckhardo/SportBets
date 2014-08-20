4fa14d49f5ccccc6b93f6daa6e2149a3d107e81e
ECKHARDO <eckhard.kirschning@freenet.de>
2014-08-20 21:55:35 +0200

    THIRD commit Wizard 20/08/2014 (REFACTORING) : Use multiple controllers for flow; Some bugs fixed

--- src/main/java/com/eki/buli/bhtpostgres/CompRoundController.java
+++ src/main/java/com/eki/buli/bhtpostgres/CompRoundController.java
@@ -99,6 +99,7 @@ public class CompRoundController implements Serializable {
         if (!JsfUtil.isValidationFailed()) {
             selected = null; // Remove selection
             items = null;    // Invalidate list of items to trigger re-query.
+            itemsForComp=null;
         }
     }
 
